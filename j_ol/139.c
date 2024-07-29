57b69ec33863b775d89af72a5dbcc389e2c3a7a2
Jeong Seungjin <malbadac@gmail.com>
2024-07-29 19:07:36 +0900

    Add files via upload

---
+++ j_ol/139.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int number1 = 0;
+    int number2 = 0;
+
+    scanf("%d %d", &number1, &number2);
+    if (number1 >= 2 && number1 <= 9 && number2 >= 2 && number2 <= 9)
+    {
+        if (number1 > number2)
+        {
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+        }
+    }
+
+    for (int i = 1; i <= 9; i++)
+    {
+        for (int j = number2; j >= number1; j--)
+        {
+            printf("%d * %d = %2d   ", j, i, i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
